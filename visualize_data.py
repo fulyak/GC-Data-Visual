b2297467a6da5150aba8d5252ad2ec358106a94b
fulyak <fulyakiroglu@gmail.com>
2022-06-22 15:47:57 -0500

    testing functions

--- visualize_data.py
+++ visualize_data.py
@@ -1,6 +1,14 @@
 import matplotlib.pyplot as plt 
 import pandas as pd
 # given 1 parameter list, make histogram
+
+
+def read_data(X,Y,file_name='mean_radial_velocities.dat'):
+    file = open(file_name,'r')
+    x,y = pd.read_csv(file,skiprows=2,header=None,delim_whitespace=True,usecols=(X,Y)).values.T
+    return x,y
+
+# given 1 parameter list, make histogram
 def make_hist(param):
 	plt.hist(param)
 	plt.show()
@@ -10,16 +18,9 @@ def make_scatter(param1, param2):
 	plt.scatter(param1, param2)
 	plt.show()
 
-def read_data(X,Y,file_name='mean_radial_velocities.dat'):
-    file = open(file_name,'r')
-    x,y = pd.read_csv(file,skiprows=2,header=None,delim_whitespace=True,usecols=(X,Y)).values.T
-    return x,y
 # tests
-p = [3, 2, 4, 3, 1, 2]
-q = [4, 2, 3, 1, 2, 3]
+x, y = read_data(1,2)
+#make_hist(p)
+make_scatter(x, y)
 
-make_hist(p)
-make_scatter(p, q)
-
-x, y = read_data(0,1)
 print(x,y)
