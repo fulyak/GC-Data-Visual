1b3ebe2a30a1868fcd188af58ff85c22cc676014
Rachel Zhang <rachelzhang@dhcp-10-105-5-253.wireless.northwestern.private>
2022-06-22 15:23:21 -0500

    adding plot function

--- visualize_data.py
+++ visualize_data.py
@@ -1,2 +1,19 @@
-import numpy as np
-x=np.array([0,1])
+import matplotlib.pyplot as plt 
+
+# given 1 parameter list, make histogram
+def make_hist(param):
+	plt.hist(param)
+	plt.show()
+
+# given 2 general parameter lists, make scatter plots of them plotted against each other
+def make_scatter(param1, param2):
+	plt.scatter(param1, param2)
+	plt.show()
+
+
+# tests
+p = [3, 2, 4, 3, 1, 2]
+q = [4, 2, 3, 1, 2, 3]
+
+make_hist(p)
+make_scatter(p, q)
