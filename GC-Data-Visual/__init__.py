d6b1fe05f1b03c6c1fc0de7d5c1264a6821a479f
Rachel Zhang <rachelzhang@Rachels-MacBook-Pro.local>
2022-06-23 17:04:32 -0500

    added files

---
+++ GC-Data-Visual/__init__.py
@@ -0,0 +1,7 @@
+import os
+
+__version__ = '1.0.0'
+
+# set Python env variable to keep track of example data dir
+orbitize_dir = os.path.dirname(__file__)
+DATADIR = os.path.join(orbitize_dir, 'gc_data/')
