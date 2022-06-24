a5d3094d335adec1e4f5794bf223e073ec0a0902
fulyak <fulyakiroglu@gmail.com>
2022-06-24 12:59:30 -0500

    added csv file to SOURCE.txt

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, find_packages
 setup(
 	name="GCDataVis",
-	version="1.0.0",
-	packages=find_packages()
+	version="1.0.2",
+	packages=find_packages(),
+	include_package_data=True
 )
